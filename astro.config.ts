c9dadcda1650e5b8ff6612c7fced7cd9831dc48e
Steffen Beisenherz <git@beisenherz.dev>
2025-08-05 10:38:17 +0200

    move to free vercel hosting

--- astro.config.ts
+++ astro.config.ts
@@ -3,13 +3,18 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://beisenherz.dev",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [sitemap()],
+
   markdown: {
     shikiConfig: {
       themes: {
@@ -18,4 +23,8 @@ export default defineConfig({
       },
     },
   },
+
+  adapter: vercel({
+    maxDuration: 20,
+  }),
 });
---
+++ src/pages/.well-known/webfinger.ts
@@ -0,0 +1,34 @@
+import { APIContext } from "astro";
+
+export const prerender = false;
+
+export async function GET({ url }: APIContext) {
+  const params = new URL(url).searchParams;
+  const resource = params.get("resource");
+
+  if (!resource) {
+    console.info("No resource path was given!");
+    return new Response(null, { status: 400 });
+  }
+
+  if (!resource.endsWith("beisenherz.dev")) {
+    console.info("No valid domain was requested");
+    return new Response(null, { status: 404 });
+  }
+
+  return new Response(
+    JSON.stringify({
+      subject: resource,
+      properties: { "http://webfinger.example/ns/name": "Steffen Beisenherz" },
+      links: [
+        {
+          rel: "http://openid.net/specs/connect/1.0/issuer",
+          href: "https://auth.beisenherz.dev",
+        },
+      ],
+    }),
+    {
+      headers: { "Content-Type": "application/jrd+json" },
+    },
+  );
+}
